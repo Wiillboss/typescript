844857318628d1e9cde5f2b9eb5c12ac19314e6c
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-04-11 22:07:40 -0300

    declaração de variável

---
+++ src/types_primitivos.ts
@@ -0,0 +1,11 @@
+//forma JS de declarar variáveis
+/*
+let nome = 'Willian'
+let idade = 30
+let ativo = true
+*/
+
+//forma TS de declarar variáveis
+let nome: string = 'Willian';
+let idade: number = 30;
+let ativo: boolean = true;
