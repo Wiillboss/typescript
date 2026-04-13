6e77b6b3f06528fc9a65fedb46b66da9b0284f29
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-04-12 21:51:47 -0300

    melhorado o comentário

--- src/type_parametros_funcoes.ts
+++ src/type_parametros_funcoes.ts
@@ -8,9 +8,10 @@ function firstLetterUpperCase(nome4 : string) {
 firstLetterUpperCase('willian'); // Retorna 'Willian', caso tente passar um número ou outro tipo de valor, o TypeScript irá dar um erro, pois o tipo do parâmetro é string e não number ou outro tipo. 
 
 //a função primeiraLetraMaiuscula é semelhante à função firstLetterUpperCase, mas utiliza métodos diferentes para obter a primeira letra e o restante do nome. O resultado final é o mesmo, ou seja, a string com a primeira letra em maiúscula. O TypeScript também garante que o parâmetro nome5 seja do tipo string, evitando erros em tempo de execução caso seja passado um valor de outro tipo.
-function primeiraLetraMaiuscula(nome5) {
+function primeiraLetraMaiuscula(nome5: any) {
     let primeiraLetra = nome5.charAt(0).toUpperCase(); // Obtém a primeira letra do nome e converte para maiúscula
     return primeiraLetra + nome5.substring(1); //essa linha faz a concatenação da primeira letra maiúscula com o restante do nome, utilizando o método substring para obter o restante do nome a partir do índice 1. O resultado final é a string com a primeira letra em maiúscula.
+    //Foi usando o type any no paremetro da variável para sumir o erro, entretanto se deixar sem type ele será considerado do tipo any, e o resultado final será o mesmo, ou seja, a string com a primeira letra em maiúscula. O TypeScript não irá dar um erro, mas o resultado final pode ser inesperado caso seja passado um valor de outro tipo, como um número ou um objeto, pois o método charAt e substring são aplicados a uma string, e outros tipos de valores podem ser convertidos para string antes de serem processados.
 }
 
 primeiraLetraMaiuscula('willian'); // Retorna 'Willian'
