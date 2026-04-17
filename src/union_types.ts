61165fc2150dd6b6abeddde6170ef42fee4feb33
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-04-16 21:34:36 -0300

    union_types

---
+++ src/union_types.ts
@@ -0,0 +1,21 @@
+// Union Types. Os tipos de união permitem que uma variável possa ser de mais de um tipo. Isso é útil quando você deseja que uma variável possa aceitar diferentes tipos de valores.
+let idaadee: string | number = 90;
+
+idaadee = document.getElementById('idade')?.textContent || 'Idade não encontrada';
+
+
+// Exemplo de função que aceita um tipo de união
+function imprimirIdade(idade: string | number) {
+     
+    // Daria um Erro: A propriedade 'toUpperCase' não existe no tipo 'string | number'. O TypeScript não sabe se 'idaadee' é uma string ou um número, então ele não permite acessar métodos específicos de um tipo sem uma verificação de tipo.  
+    
+    if (typeof idaadee === 'string') {
+        console.log(idaadee.toUpperCase());
+    } else {
+        console.log(`Idade: ${idade}`);
+    }
+}
+imprimirIdade(idaadee); 
+imprimirIdade(25);
+imprimirIdade('Trinta');    
+imprimirIdade('30');    
