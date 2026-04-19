29e8dddff6f0031c756ddce6583dc7231d2a0365
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-04-18 21:33:56 -0300

    types assertions

---
+++ src/types_assertions.ts
@@ -0,0 +1,8 @@
+let idadeField = document.getElementById('idade') as HTMLInputElement;
+
+//...
+
+console.log(idadeField.value);
+
+//A propriedade value não EXISTE no TYPE HTMLELEMENT
+//A propriedade value é exclusivo do OBJETO do tipo HTMLImputElemente
